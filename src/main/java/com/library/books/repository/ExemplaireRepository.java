62febeb93930d8c1295663db06be2b5d4749578c
Coubiac <benoit.grisot@gmail.com>
2020-06-15 17:26:41 +0200

    add find exemplaire by barcode

--- src/main/java/com/library/books/repository/ExemplaireRepository.java
+++ src/main/java/com/library/books/repository/ExemplaireRepository.java
@@ -2,9 +2,12 @@ package com.library.books.repository;
 
 import com.library.books.entity.Exemplaire;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "exemplaires")
 public interface ExemplaireRepository extends PagingAndSortingRepository<Exemplaire, Integer> {
-    Exemplaire findOneByBarcode(Integer barcode);
+    @RestResource(path = "byBarcode", rel="find by barcode")
+    Exemplaire findOneByBarcode(@Param("barcode") Integer barcode);
 }
